26b2faf18679adce6b10733282e7fd477b1f9177
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-10-06 22:53:06 +0530

    Update UserController.java

--- Controller/UserController.java
+++ Controller/UserController.java
@@ -1,84 +1,83 @@
-package com.billingapplication.controller;
+package com.billingapplication.service;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
-import com.billingapplication.entity.Admin;
 import com.billingapplication.entity.User;
 import com.billingapplication.exception.RecordNotFoundException;
 import com.billingapplication.repository.UserRepository;
-import com.billingapplication.service.UserService;
-
-import jakarta.servlet.http.HttpSession;
-
-@RestController
-public class UserController {
-	@Autowired
-	private UserService userSer;
+@Service
+public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository userRepo;
-
-	@PostMapping("/api/admin/createAccountant")
-	public ResponseEntity<String> createAccountant(@RequestBody User us, HttpSession session) {
-		if (session.getAttribute("adminloginsession") != null) {
-			Boolean emailExists = userSer.existsEmail(us.getEmail());
-			if (emailExists) {
-				return new ResponseEntity<>("Email already exists", HttpStatus.CONFLICT);
-			} else {
-				User newUser = userSer.createAccountant(us);
-				if (newUser != null) {
-					return new ResponseEntity<>("Account created successfully", HttpStatus.CREATED);
-				} else {
-					return new ResponseEntity<>("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
-				}
-			}
-		}
-		return new ResponseEntity<>("Unauthorized access", HttpStatus.UNAUTHORIZED);
+	@Override
+	public User createAccountant(User user) {
+		user.setRole("ROLE_ACCOUNTANT");
+		User newAcc=userRepo.save(user);
+		return newAcc;
 	}
 
-	@PutMapping("/api/admin/updateAccountant/{id}")
-	public ResponseEntity<?> getUpdateAccountant(@PathVariable("id") int id, HttpSession session,
-			@RequestBody User us) {
-		if (session.getAttribute("adminloginsession") != null) {
-			us.setId(id);
-			return ResponseEntity.ok().body(userSer.updateAccountant(us));
-		}
-		return new ResponseEntity<>("Unauthorized access", HttpStatus.UNAUTHORIZED);
+	@Override
+	public boolean existsEmail(String email) {
+		return userRepo.existsByEmail(email);
 	}
 
-	@GetMapping("/api/admin/displayAcc")
-	public ResponseEntity<?> getAllAccountants(HttpSession session) {
-		if (session.getAttribute("adminloginsession") != null) {
-			List<User> users = userSer.getAllAccountant(new User());
-			return new ResponseEntity<>(users, HttpStatus.OK);
+	@Override
+	public User updateAccountant(User user) {
+		Optional<User> u=userRepo.findById(user.getId());
+		if(u.isPresent()) {
+			User us=u.get();
+			us.setName(user.getName());
+			us.setCity(user.getCity());
+			us.setEmail(user.getEmail());
+			us.setMobileNumber(user.getMobileNumber());
+			us.setRole("ROLE_ACCOUNTANT");
+			return userRepo.save(us);
 		}
-		return new ResponseEntity<>("Unauthorized access", HttpStatus.UNAUTHORIZED);
+		else {
+            throw new RecordNotFoundException("Accountant with id " + user.getId() + " not found");
+        }
 	}
 
-	@DeleteMapping("/api/admin/deleteAcc/{id}")
-	public ResponseEntity<String> deleteAccountant(@PathVariable("id") int id, HttpSession session) {
-		if (session.getAttribute("adminloginsession") != null) {
-			userSer.deleteAccountant(id);
-			return new ResponseEntity<>("Accountant deleted successfully", HttpStatus.OK);
-		}
-		return new ResponseEntity<>("Unauthorized access", HttpStatus.UNAUTHORIZED);
+	@Override
+	public List<User> getAllAccountant(User user) {
+		return userRepo.findAll();
 	}
 
-	@PostMapping("/api/users/accLogin")
-	public ResponseEntity<String> loginAccountant(@RequestBody User accountantCredentials, HttpSession session) {
-		String email=accountantCredentials.getEmail();
-		String password=accountantCredentials.getPassword();
-		User user=userSer.validateAccountant(email, password);
-		if (user != null) {
-			session.setAttribute("accountantlogin", user);
-			return new ResponseEntity<>("Login successful", HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>("Invalid email or password", HttpStatus.UNAUTHORIZED);
+	@Override
+	public void deleteAccountant(int id) {
+		Optional<User> u=userRepo.findById(id);
+		if(u.isPresent()) {
+			User us=u.get();
+			userRepo.delete(us);
+		}
+		else {
+			throw new RecordNotFoundException("Accountant with id " + id + " not found");
 		}
 	}
+
+	@Override
+	public User validateAccountant(String email,String password) {
+		User u=userRepo.findByEmail(email);
+		if(u!=null && u.getPassword().equals(password)) {
+			return u;
+		}
+		return null;
+	}
+
+	@Override
+	public String getEmailById(int id) {
+		Optional<User> u=userRepo.findById(id);
+		if(u.isPresent()) {
+			User us=u.get();
+			return us.getEmail();
+		}
+		return null;
+	}
+
+	
+
 }
