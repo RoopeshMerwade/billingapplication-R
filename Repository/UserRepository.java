557449e37eb74c6c872af5f09f01ddcbfae8bf8d
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-10-10 16:55:33 +0530

    Update UserRepository.java

--- Repository/UserRepository.java
+++ Repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.billingapplication.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.billingapplication.entity.Admin;
+import com.billingapplication.entity.User;
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
-	public Admin findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer>{
 	public boolean existsByEmail(String email);
+	public Optional<User> findById(int id);
+	public User findByEmail(String email);
 }
