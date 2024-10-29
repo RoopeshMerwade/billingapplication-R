75df9b8525b84a6969d9844653b6065a4d95a926
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-10-29 21:27:08 +0530

    Delete service directory

--- service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.billingapplication.service;
-
-import com.billingapplication.entity.Admin;
-
-public interface AdminService {
-	public Admin validateAdmin(String email, String password);
-	public boolean existsEmail(String email);
-}
