db6da8b48f7ae575e408529ff11c12438a201b0b
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-10-29 21:53:18 +0530

    Update AnalyticsPage.jsx

--- React Frontend code/src/pages/AnalyticsPage.jsx
+++ React Frontend code/src/pages/AnalyticsPage.jsx
@@ -1,12 +1,6 @@
 import Header from "../components/common/Header";
 
 import OverviewCards from "../components/analytics/OverviewCards";
-// import RevenueChart from "../components/analytics/RevenueChart";
-// import ChannelPerformance from "../components/analytics/ChannelPerformance";
-// import ProductPerformance from "../components/analytics/ProductPerformance";
-// import UserRetention from "../components/analytics/UserRetention";
-// import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
-// import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
 import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
 import DailyOrders from "../components/orders/DailyOrders";
 
@@ -17,18 +11,11 @@ const AnalyticsPage = () => {
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
         <OverviewCards />
-        {/* <RevenueChart /> */}
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
-          {/* <ChannelPerformance />
-					<ProductPerformance /> */}
           <CategoryDistributionChart />
           <DailyOrders />
-          {/* <UserRetention /> */}
-          {/* <CustomerSegmentation /> */}
         </div>
-
-        {/* <AIPoweredInsights /> */}
       </main>
     </div>
   );
